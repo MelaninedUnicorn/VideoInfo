64d5ad6471fe55153e6f73cc8918faab81ee8a1e
Mugisha J Kakou <mugisha@joycava.com>
2021-06-01 18:39:57 +0200

    Electron require() is not defined

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const electron = require("electron");
-
+const ffmpeg = require("fluent-ffmpeg");
 const { app, BrowserWindow, ipcMain } = electron;
 
 app.on("ready", () => {
-    console.log("Hello Mugisha <3")
-	const mainWindow = new BrowserWindow({});
-    mainWindow.loadURL(`file://${__dirname}/index.html`);
+	console.log("Hello Mugisha <3");
+	const mainWindow = new BrowserWindow({
+		webPreferences: { nodeIntegration: true },
+	});
+	mainWindow.loadURL(`file://${__dirname}/index.html`);
 });
 
-ipcMain.on('video:submit', ()=>{
-
+ipcMain.on("video:submit", (event, path) => {
+	ffmpeg.ffprobe(path, (err, metadata) => {
+		console.log("Video duration is : ", metadata.format.duration);
+	});
 });
